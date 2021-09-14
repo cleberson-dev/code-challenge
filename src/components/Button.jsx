b6d297e1a82813f0e6364a11686e1bf682045ec1
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2021-09-14 14:59:18 -0300

    Create Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+export default function Button({ icon, status, onClick, label = 'Click here' }) {
+  const colors = {
+    success: '#8FDA58',
+    danger: '#F8635A'
+  };
+
+  const clickHandler = onClick || (() => {});
+
+  return (
+    <button 
+      onClick={clickHandler}
+      style={{ backgroundColor: colors[status] || colors.success }} 
+      className="rounded-full outline-none border-0 flex justify-center items-center w-12 h-12"
+    >
+      <img src={icon} alt={label} />
+    </button>
+  )
+}
