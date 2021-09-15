4af157c431e9388997e65dad37aca64ec251859d
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2021-09-15 10:00:22 -0300

    WIP: List pokemon teams

---
+++ src/components/Team.jsx
@@ -0,0 +1,14 @@
+import PokemonSlot from './PokemonSlot';
+
+export default function Team({ name, pokemons }) {
+  return (
+    <div>
+      <div style={{ fontFamily: 'Spartan SemiBold' }}>
+        { name }
+      </div>
+      <div className="p-8 grid grid-cols-3">
+        { pokemons.map(pokemon => <PokemonSlot key={pokemon.id} pokemon={pokemon} />) }
+      </div>
+    </div>
+  );
+}
--- src/pages/teams.jsx
+++ src/pages/teams.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 
 export default function MyTeams() {
+  const [teams, setTeams] = useState([]);
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/teams').then(res => res.json())
+      .then(teams => {
+        setTeams(teams);
+        const pokemonIds = teams
+          .map(team => team.pokemons.map(({ id }) => id))
+          .reduce((flat, val) => flat.concat(val), []);
+        const uniquePokemonIds = [...new Set(pokemonIds)];
+
+        return Promise.all(uniquePokemonIds.map(pokemonId => fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)));
+      })
+      .then(pokemons => {
+        setPokemons(pokemons);
+      });
+  }, []);
+  
+  
+  
   return (
     <div>
       <Header />
+      { teams.length > 0 && teams.map(team => <Team key={team.id} name={team.name} pokemons={team.pokemons.map(pokemon => pokemons.find(p => p.id === pokemon.id))} />) }
     </div>
   );
 }
