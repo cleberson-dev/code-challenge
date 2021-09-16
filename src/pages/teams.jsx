8526a7b23b799e51188741fdb7976980b46b50c4
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2021-09-16 12:15:21 -0300

    Add infinite scrolling on teams page

--- src/pages/teams.jsx
+++ src/pages/teams.jsx
@@ -1,15 +1,22 @@
 import { useEffect, useState } from "react";
 import Team from "../components/Team";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function MyTeams() {
   const [teams, setTeams] = useState([]);
   const [pokemons, setPokemons] = useState([]);
+  const [hasMoreTeams, setHasMoreTeams] = useState(true);
+  const size = 4;
 
   useEffect(() => {
-    fetch('/api/teams').then(res => res.json())
-      .then(teams => {
-        setTeams(teams);
-        const pokemonIds = teams
+    fetch(`/api/teams?size=${size}`).then(res => res.json())
+      .then(({ results, next }) => {
+        setTeams(results);
+        if (!next) {
+          setHasMoreTeams(false);
+        }
+
+        const pokemonIds = results
           .map(team => team.pokemons.map(({ id }) => id))
           .reduce((flat, val) => flat.concat(val), []);
         const uniquePokemonIds = [...new Set(pokemonIds)];
@@ -20,21 +27,60 @@ export default function MyTeams() {
         setPokemons(pokemons);
       });
   }, []);
+
+  function loadMoreTeams() {
+    fetch(`/api/teams?size=${size}&offset=${teams.length}`).then(res => res.json())
+      .then(({ results: newTeams, next }) => {
+        setTeams([
+          ...teams,
+          ...newTeams,
+        ]);
+        if (!next) {
+          setHasMoreTeams(false);
+        }
+
+        const pokemonIds = newTeams
+          .map(team => team.pokemons.map(({ id }) => id))
+          .reduce((flat, val) => flat.concat(val), []);
+        const uniquePokemonIds = [...new Set(pokemonIds)];
+        const missingPokemonIds = uniquePokemonIds.filter(pokemonId => pokemons.every(p => p.id !== pokemonId));
+
+        return Promise.all(missingPokemonIds.map(pokemonId => fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`).then(res => res.json())));
+      })
+      .then(newPokemons => {
+        setPokemons([
+          ...pokemons,
+          ...newPokemons
+        ]);
+      });
+  }
   
   
   
   return (
     <>
-      { teams.length > 0 && pokemons.length > 0 && (
-        <ul>
-          {teams.map(team => (
-            <li key={team.id} className="mb-8 pb-4 last:border-transparent border-b border-solid border-black">
-              <Team name={team.name} pokemons={team.pokemons.map(pokemon => pokemons.find(p => p.id === pokemon.id))} /> 
-            </li>
-          ))
-          }
-        </ul>
-      )}
+      <InfiniteScroll
+        dataLength={teams.length}
+        next={loadMoreTeams}
+        hasMore={hasMoreTeams}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        { teams.length > 0 && pokemons.length > 0 && (
+          <ul>
+            {teams.map(team => (
+              <li key={team.id} className="mb-8 pb-4 last:border-transparent border-b border-solid border-black">
+                <Team name={team.name} pokemons={team.pokemons.map(pokemon => pokemons.find(p => p.id === pokemon.id)).filter(pokemon => !!pokemon)} /> 
+              </li>
+            ))
+            }
+          </ul>
+        )}
+      </InfiniteScroll>
     </>
   );
 }
