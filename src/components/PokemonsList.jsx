b179801a2e6296ae8a909a28978ee49dfd5a124b
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2021-09-16 12:40:03 -0300

    Style scrollbar on PokemonsList

--- src/components/PokemonsList.jsx
+++ src/components/PokemonsList.jsx
@@ -4,6 +4,7 @@ import Pokemon from './Pokemon';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import confirmIcon from '../../public/confirm-icon.svg';
 import { AppContext } from '../pages/_app';
+import { css } from '@emotion/css';
 
 export default function PokemonsList({ pokemons, next }) {
   const { isPokemonAddedToSlot, availableSlots, removePokemonFromSlot, addPokemonToOpenSlot } = useContext(AppContext);
@@ -25,33 +26,51 @@ export default function PokemonsList({ pokemons, next }) {
     addPokemonToOpenSlot(pokemon); 
   }
 
+  const classes = `${css`
+    &::-webkit-scrollbar {
+      background-color: transparent;
+      width: 5px;
+    }
+
+    &::-webkit-scrollbar-track {
+      background-color: transparent;
+    }
+    
+    &::-webkit-scrollbar-thumb {
+      background-color: #C4C4C4; 
+      border-radius: 5px;
+    }
+  `} overflow-x-hidden overflow-y-auto h-64`;
+
   if (pokemons.length === 0) return <div>EMPTY</div>;
   return (
-    <InfiniteScroll
-      dataLength={pokemons.length}
-      next={next}
-      hasMore={true}
-      loader={<h4>Loading...</h4>}
-      endMessage={
-        <p style={{ textAlign: 'center' }}>
-          <b>Yay! You have seen it all</b>
-        </p>
-      }
-    >
-      <div className="grid grid-cols-4" style={{ columnGap: '2rem', rowGap: '2rem' }}>
-        {pokemons.map(pokemon => {
-          const classes = classNames({
-            'relative': true,
-            'cursor-pointer': isPokemonAddedToSlot(pokemon) || availableSlots > 0
-          });
-          return (
-            <div key={pokemon.id} className={classes} onClick={isPokemonAddedToSlot(pokemon) || availableSlots > 0 ? (() => pokemonClickHandler(pokemon)) : undefined}>
-              <Pokemon pokemon={pokemon} /> 
-              { isPokemonAddedToSlot(pokemon) && addedIndicatorElement }
-            </div>
-          );
-        })}
-      </div>
-    </InfiniteScroll>
+    <div className={classes}>
+      <InfiniteScroll
+        dataLength={pokemons.length}
+        next={next}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: 'center' }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        <div className="grid grid-cols-4" style={{ columnGap: '2rem', rowGap: '2rem' }}>
+          {pokemons.map(pokemon => {
+            const classes = classNames({
+              'relative': true,
+              'cursor-pointer': isPokemonAddedToSlot(pokemon) || availableSlots > 0
+            });
+            return (
+              <div key={pokemon.id} className={classes} onClick={isPokemonAddedToSlot(pokemon) || availableSlots > 0 ? (() => pokemonClickHandler(pokemon)) : undefined}>
+                <Pokemon pokemon={pokemon} /> 
+                { isPokemonAddedToSlot(pokemon) && addedIndicatorElement }
+              </div>
+            );
+          })}
+        </div>
+      </InfiniteScroll>
+    </div>
   );
 }
